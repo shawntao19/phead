e88a051333de64c1991104751ccff3168d070ad3
happy <happy@192.168.0.100>
2017-10-16 18:08:59 +0800

    第二天版本 有excel下载测试模式，有基本的页面处理完成。 controller

---
+++ src/java/controller/HelloController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloController implements  Controller{
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message", "Hello World!你好");  
+        mv.setViewName("hello");
+        return mv;
+    }
+
+}
